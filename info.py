75ab13cbc2ebb51b1b20612a5f00906a819a3d59
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2024-03-30 12:57:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,67 +17,34 @@ def is_valid_ip(ip):
     return re.match(ip_pattern, ip) is not None
 
 # Bot information
-API_ID = environ.get('API_ID', '')
-if len(API_ID) == 0:
-    print('Error - API_ID is missing, exiting now')
-    exit()
-else:
-    API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', '')
-if len(API_HASH) == 0:
-    print('Error - API_HASH is missing, exiting now')
-    exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-if len(BOT_TOKEN) == 0:
-    print('Error - BOT_TOKEN is missing, exiting now')
-    exit()
+API_ID = environ.get('API_ID', '8858279')
+API_HASH = environ.get('API_HASH', 'ef28c3f458143cbcb4271a98a2e9d596')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7021666831:AAEHYHSjuHvxxSDO-jaOBenqNOdL2z8AcHA')
 PORT = int(environ.get('PORT', '80'))
 
 # Bot pics
 PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '')
-if len(ADMINS) == 0:
-    print('Error - ADMINS is missing, exiting now')
-    exit()
-else:
-    ADMINS = [int(admins) for admins in ADMINS.split()]
+ADMINS = environ.get('ADMINS', '1485166069')
+ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-if len(INDEX_CHANNELS) == 0:
-    print('Info - INDEX_CHANNELS is empty')
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '').split()]
-if len(AUTH_CHANNEL) == 0:
-    print('Info - AUTH_CHANNEL is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '')
-if len(LOG_CHANNEL) == 0:
-    print('Error - LOG_CHANNEL is missing, exiting now')
-    exit()
-else:
-    LOG_CHANNEL = int(LOG_CHANNEL)
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1001998108714').split()]
+AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002138056667').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001998108714'))
 IS_FSUB = is_enabled('IS_FSUB', True)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
-if len(SUPPORT_GROUP) == 0:
-    print('Error - SUPPORT_GROUP is missing, exiting now')
-    exit()
-else:
-    SUPPORT_GROUP = int(SUPPORT_GROUP)
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002037930063')
+SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # for chatGPT
 OPENAI_API = environ.get('OPENAI_API', '')
-if len(OPENAI_API) == 0:
-    print('Info - OPENAI_API is empty')
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
-if len(DATABASE_URL) == 0:
-    print('Error - DATABASE_URL is missing, exiting now')
-    exit()
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://zoro:lost@cluster0.4zwdyde.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sidd")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
@@ -89,22 +56,22 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
 
 # Bot settings
-DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-MAX_BTN = int(environ.get('MAX_BTN', 10))
+DELETE_TIME = int(environ.get('DELETE_TIME', '3600'))
+CACHE_TIME = int(environ.get('CACHE_TIME', '300'))
+MAX_BTN = int(environ.get('MAX_BTN', '10'))
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'english hindi telugu tamil kannada malayalam').split()]
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link")
 SHORTLINK_API = environ.get("SHORTLINK_API", "36f1ae74ba1aa01e5bd73bdd0bc22aa915443501")
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', '86400'))
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
 
 # boolean settings
 IS_VERIFY = is_enabled('IS_VERIFY', False)
-AUTO_DELETE = is_enabled('AUTO_DELETE', False)
-WELCOME = is_enabled('WELCOME', False)
+AUTO_DELETE = is_enabled('AUTO_DELETE', True)
+WELCOME = is_enabled('WELCOME', True)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
 LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
 LINK_MODE = is_enabled("LINK_MODE", True)
@@ -117,22 +84,13 @@ PAYMENT_QR = environ.get('PAYMENT_QR', 'http://graph.org/file/cacbbea472e5a48ce0
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
-if len(BIN_CHANNEL) == 0:
-    print('Error - BIN_CHANNEL is missing, exiting now')
-    exit()
+BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1001998108714"))
+URL = environ.get("URL", "pingme-sidd-2005.koyeb.app")
+if URL.startswith(('https://', 'http://')):
+    if not URL.endswith("/"):
+        URL += '/'
+elif is_valid_ip(URL):
+    URL = f'http://{URL}/'
 else:
-    BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
-if len(URL) == 0:
-    print('Error - URL is missing, exiting now')
+    print('Error - URL is not valid, exiting now')
     exit()
-else:
-    if URL.startswith(('https://', 'http://')):
-        if not URL.endswith("/"):
-            URL += '/'
-    elif is_valid_ip(URL):
-        URL = f'http://{URL}/'
-    else:
-        print('Error - URL is not valid, exiting now')
-        exit()
